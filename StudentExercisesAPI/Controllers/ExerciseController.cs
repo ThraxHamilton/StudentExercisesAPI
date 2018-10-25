5f5f25fac18b8fd944333c933db6db40f992e0ce
David <dntaylor14@gmail.com>
2018-10-25 15:53:35 -0500

    controlla

---
+++ StudentExercisesAPI/Controllers/ExerciseController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExercisesAPI.Data;
+
+namespace StudentExercisesAPI.Controllers
+{   
+    //Specify route to call from API
+    [Route("api/[controller]")]
+    [ApiController]
+    //Create class for controller
+    public class ExerciseController : ControllerBase
+    {
+        // Shows connection to db       vvvvvv
+        private readonly IConfiguration _config;
+
+        public ExerciseController(IConfiguration config)
+        {
+            _config = config;
+        }
+        // Create connection to sql
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string q)
+        {
+            //Query sql server to select (a,b,c,) from table
+            string sql = @"
+            SELECT
+                e.Id,
+                e.Name,
+                e.Language
+            FROM Exercise e
+            ";
+            if (q != null)
+            {
+                string isQ = $@"
+                    AND e.Name LIKE '%{q}%'
+                    OR e.Language LIKE '%{q}%'
+                ";
+                sql = $"{sql} {isQ}";
+            }
+            Console.WriteLine(sql);
+            //Using database 
+            using (IDbConnection conn = Connection)
+            {
+
+                IEnumerable<Exercise> exercises = await conn.QueryAsync<Exercise>(
+                    sql
+                );
+                return Ok(exercises);
+            }
+        }
+
+    }
+
+}
---
+++ StudentExercisesAPI/Controllers/InstructorController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExercisesAPI.Data;
+
+namespace StudentExercisesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructorController : ControllerBase
+    {
+        
+            // Shows connection to db       vvvvvv
+            private readonly IConfiguration _config;
+
+            public InstructorController(IConfiguration config)
+            {
+                _config = config;
+            }
+            // Create connection to sql
+            public IDbConnection Connection
+            {
+                get
+                {
+                    return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+                }
+            }
+            [HttpGet]
+            public async Task<IActionResult> Get(string q)
+            {
+                //Query sql server to select (a,b,c,) from table
+                string sql = @"
+            SELECT
+                i.Id,
+                i.FirstName,
+                i.LastName,
+                i.SlackHandle,
+                i.Specialty,
+                i.Cohort
+            FROM Instructor i
+            ";
+                if (q != null)
+                {
+                    string isQ = $@"
+                    AND e.FirstName LIKE '%{q}%'
+                    OR e.Cohort LIKE '%{q}%'
+                ";
+                    sql = $"{sql} {isQ}";
+                }
+                Console.WriteLine(sql);
+                //Using database 
+                using (IDbConnection conn = Connection)
+                {
+
+                    IEnumerable<Instructor> instructors = await conn.QueryAsync<Instructor>(
+                        sql
+                    );
+                    return Ok(instructors);
+                }
+            }
+
+        }
+    }
